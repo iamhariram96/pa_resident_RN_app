e364c356d5ec1fc96c50c3ae094eac98074c7a54
“hariram” <hariram@crayond.co>
2023-10-28 13:19:08 +0530

    resident app build fix issues of location permistion and logout

--- src/screen/webScreen.jsx
+++ src/screen/webScreen.jsx
@@ -32,6 +32,65 @@ const INJECTED_JAVASCRIPT = `(function() {
     window.ReactNativeWebView.postMessage(getItemLocalStorage);
 })();`;
 
+export const getGeoLocationJS = () => {
+    const getCurrentPosition = `
+      navigator.geolocation.getCurrentPosition = (success, error, options) => {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ event: 'getCurrentPosition', options: options }));
+  
+        window.addEventListener('message', (e) => {
+          let eventData = {}
+          try {
+            eventData = JSON.parse(e.data);
+          } catch (e) {}
+  
+          if (eventData.event === 'currentPosition') {
+            success(eventData.data);
+          } else if (eventData.event === 'currentPositionError') {
+            error(eventData.data);
+          }
+        });
+      };
+      true;
+    `;
+  
+    const watchPosition = `
+      navigator.geolocation.watchPosition = (success, error, options) => {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ event: 'watchPosition', options: options }));
+  
+        window.addEventListener('message', (e) => {
+          let eventData = {}
+          try {
+            eventData = JSON.parse(e.data);
+          } catch (e) {}
+  
+          if (eventData.event === 'watchPosition') {
+            success(eventData.data);
+          } else if (eventData.event === 'watchPositionError') {
+            error(eventData.data);
+          }
+        });
+      };
+      true;
+    `;
+  
+    const clearWatch = `
+      navigator.geolocation.clearWatch = (watchID) => {
+        window.ReactNativeWebView.postMessage(JSON.stringify({ event: 'clearWatch', watchID: watchID }));
+      };
+      true;
+    `;
+  
+    return `
+      (function() {
+        ${getCurrentPosition}
+        ${watchPosition}
+        ${clearWatch}
+      })();
+    `;
+  };
+
+let webview = null;
+
 const isIOS = Platform.OS === 'ios';
 const { height } = Dimensions.get('window');
 
@@ -44,7 +103,7 @@ const WebScreen = (props) => {
     const [status, setStatus] = React.useState(null);
     
     const onMessage = (payload) => {
-        // console.log('payload asses', payload);
+        console.log('payload asses', payload);
     };
 
 
@@ -112,17 +171,67 @@ const WebScreen = (props) => {
     const WebviewRender = () => {
         if (webUrl?.length > 0) {
             return <WebView
-                injectedJavaScript={INJECTED_JAVASCRIPT}
-                onMessage={onMessage}
+                injectedJavaScript={getGeoLocationJS()}
+                onMessage={ event => {
+                    let data = {}
+                    try {
+                      data = JSON.parse(event.nativeEvent.data);
+                    } catch (e) {
+                      console.log(e);
+                    }
+                
+                    if (data?.event && data.event === 'getCurrentPosition') {
+                      Geolocation.getCurrentPosition((position) => {
+                        webview.postMessage(JSON.stringify({ event: 'currentPosition', data: position }));
+                      }, (error) => {
+                        webview.postMessage(JSON.stringify({ event: 'currentPositionError', data: error }));
+                      }, data.options);
+                    } else if (data?.event && data.event === 'watchPosition') {
+                      Geolocation.watchPosition((position) => {
+                        webview.postMessage(JSON.stringify({ event: 'watchPosition', data: position }));
+                      }, (error) => {
+                        webview.postMessage(JSON.stringify({ event: 'watchPositionError', data: error }));
+                      }, data.options);
+                    } else if (data?.event && data.event === 'clearWatch') {
+                      Geolocation.clearWatch(data.watchID);
+                    } 
+                  }}
+                  ref={ ref => {
+                    webview = ref;
+                    // if (onRef) {
+                    //   onRef(webview)
+                    // }
+                  }}
                 overScrollMode='never'
                 pullToRefreshEnabled={true}
-                // onLoadEnd={() => {
-                //     setVisible(false)
-                // }}
-                incognito={true}
-                cacheEnabled={false}
+
+                cacheEnabled={true}
                 // cacheMode={'LOAD_NO_CACHE'}
                 source={{ uri: webUrl }} style={{ marginTop: isIOS ? 0 : 10 }}
+
+                geolocationEnabled={true}
+                allowsInlineMediaPlayback={true}
+                mediaPlaybackRequiresUserAction={false}
+                
+                cacheMode={'LOAD_DEFAULT'}
+                sharedCookiesEnabled={true}
+                setBuiltInZoomControls={false}
+
+                useWebKit
+                originWhitelist={['*']}
+                incognito={false}
+
+                mediaCapturePermissionGrantType="grantIfSameHostElsePrompt"
+
+                geolocationPermissionRequest={(request) => {
+                  if (true) {
+                    // You have geolocation permission
+                    request.grant();
+                  } else {
+                    // You don't have geolocation permission
+                    request.deny();
+                  }
+                }}
             />
         } else {
             return <ActivityIndicator size="large" />
